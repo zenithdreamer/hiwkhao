e256cbed0c6c459eb4da9c02cc33d05cdbcc7ad3
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2024-12-22 08:13:50 +0700

    feat: make main run scanner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use scanner_lib;
+
+const DEFAULT_OUTPUT_FILE: &str = "hiwkhao.tok";
+
+// TODO: Since we only have scanner right now, we run the scanner for now.
 fn main() {
-    println!("Hello, world!");
+    let input = if let Some(file_path) = std::env::args().nth(1) {
+        std::fs::read_to_string(file_path).unwrap()
+    } else {
+        eprintln!("No input file provided.");
+        std::process::exit(1);
+    };
+
+    let result = scanner_lib::run_scanner(&input);
+    println!("{}", result.join("\n"));
+
+    let output_file = std::env::args()
+        .nth(2)
+        .unwrap_or(DEFAULT_OUTPUT_FILE.to_string());
+
+    std::fs::write(output_file, result.join("\n")).unwrap();
 }
